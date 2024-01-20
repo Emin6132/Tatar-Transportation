4af6aab9d9defa8cc225adb97e9c3f30cd975b5b
Emin <eminmuhammedinan08@gmail.com>
2024-01-20 15:04:57 +0300

    Final Code

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,9 +3,10 @@ import "../styles/Logo.css"
 const Logo = () => {
   return (
     <div className="logo-container">
-          <h2 className="logo-container-title">
-            Tatar Nakliyat
-          </h2>
+        <img src="src/images/TatarLogo2.png" className="pageLogo" />
+        <h2 className="logo-container-title">
+          Tatar Nakliyat
+        </h2>
     </div>
   )
 }
